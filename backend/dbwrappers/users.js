832e481e4426810ecad01f8265622fde6458a926
borislipo <63309073+borislipo@users.noreply.github.com>
2021-05-16 12:33:48 +0300

    added login and signup

--- backend/dbwrappers/users.js
+++ backend/dbwrappers/users.js
@@ -1,2 +1,48 @@
 const SQL = require('@nearform/sql');
 const { query } = require('../lib/db');
+const bcrypt = require('bcrypt');
+
+async function addUser(email, passwordHash, name, lastName, phone_number, location) {
+    try {
+        await query(SQL`INSERT INTO users (email, password, first_name, last_name, phone, location)
+         VALUES (${email}, ${passwordHash}, ${name}, ${lastName}, ${phone_number}, ${location} )`)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+exports.addUser = addUser
+
+
+async function getUserByEmail(email) {
+    const rows = await query(SQL`SELECT * FROM users WHERE email=${email}`)
+    console.log(rows)
+    return rows[0]
+}
+
+exports.getUserByEmail = getUserByEmail
+
+async function comapareEmailPassword(req, res, next) {
+    try {
+        const { email, password } = req.body
+        const user = await getUserByEmail(email)
+        if (!user) {
+            res.status(404).send({ message: 'user not found with tihs email' })
+            return
+        }
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (err) next(err)
+            if (result) {
+                next()
+            } else {
+                res.status(401).send({ message: 'incorrect password' })
+            }
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+exports.comapareEmailPassword = comapareEmailPassword
+
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,51 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { addUser, getUserByEmail, comapareEmailPassword } = require('../dbwrappers/users')
 const router = express.Router();
+//installed bcrypt 
+
+router.post('/',
+    async (req, res) => {
+        const { email, password, name, lastName, phone, location } = req.body
+        bcrypt.hash(password, 10, async (err, hash) => {
+            try {
+                if (err) console.error(err)
+                else {
+                    const user = await getUserByEmail(email)
+                    if (user) {
+                        res.status(403).send({ message: 'User already exists' })
+                    }
+                    await addUser(email, hash, name, lastName, phone, location)
+                    res.status(200).send({ message: "User created succesfully", email: email, name: name, lastName: lastName })
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        })
+    })
+
+router.post(
+    '/login',
+    comapareEmailPassword,
+    async (req, res, next) => {
+        try {
+            const { email } = req.body
+            const user = await getUserByEmail(email);
+            const token = `Bearer ${jwt.sign({ id: user.id }, "123456", {
+                expiresIn: '1d'
+            })}`;
+            res.status(201);
+            res.send({
+                token
+            });
+        } catch (err) {
+            res.setHeader('Access-Control-Allow-Headers: Authorization');
+            res.status(500);
+            next(err);
+        }
+    }
+);
+
 
 module.exports = router;
