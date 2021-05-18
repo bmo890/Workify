a18bf08a90a5838e53cdd18a87dd6eaea8cb7ded
Igal Sokolovsky <igal.s.1994@gmail.com>
2021-05-18 19:35:33 +0300

    Update db.js

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,6 +2,7 @@ const path = require('path');
 const mysql = require('mysql');
 const Postgrator = require('postgrator');
 //260295
+//sd
 const postgrator = new Postgrator({
 	migrationDirectory: path.resolve(__dirname, '../migrations'),
 	driver: 'mysql',
