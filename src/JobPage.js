3863d14a6ccd1d6092d8cc9dbfa8b36962f5ff12
bmo890 <77322157+bmo890@users.noreply.github.com>
2021-05-20 00:05:13 +0300

    Update JobPage.js

--- src/JobPage.js
+++ src/JobPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
-// import JobCardCarousel from './Components/JobCardCarousel'
 import Button from 'react-bootstrap/Button';
 import { getUserById, getJob } from './lib/api'
 
@@ -10,36 +9,15 @@ export default function JobPage(props) {
     
     let { jobId } = useParams()
     
-    const getJobInfo =  async (jobId) => {
-        return await getJob(jobId)
-    }
-    
     useEffect(async () => {
-        console.log('hi')
-        const currentJob = await getJobInfo(jobId)  
+        const currentJob = await getJob(jobId)  
         setJobInfo(currentJob.job)
         const jobPoster =  await getUserById(currentJob.job.userId)
         setPosterInfo(jobPoster)
     }, [])
     
-    const poster = {
-        userName: 'Frank Johnson',
-        userAvatar: 'https://randomuser.me/api/portraits/lego/5.jpg',
-        location: 'Brooklyn, New York'
-    }
-    
-    const job = {
-        poster: poster,
-        jobName: 'Help me fix my door!',
-        datePosted: '3 days ago',
-        image: [],
-        jobCategory: ['Woodworking', 'Handyman'],
-        jobDescription: 'I broke my door handle while chasing my dog around the apartment. Looks like the handle needs to be replaced entirely. Not sure if I need a completely new door or not.'
-        
-    }
-    
-    const mapTags = job.jobCategory.join(`, `)
-    console.log(jobInfo)
+  
+console.log(jobInfo)
     console.log(posterInfo)
     
     let timeStamp = new Date (jobInfo.createdDate)
@@ -54,7 +32,6 @@ export default function JobPage(props) {
         <h4>Category: {jobInfo.category}</h4>
         <div style={{ width: '50rem' }}>
             <img src={jobInfo.picture ? jobInfo.picture : 'https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg'} />
-            {/* <JobCardCarousel picture={jobInfo.picture.length > 0 ? jobInfo.picture : 'https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg'} /> */}
             <div style={{ display: 'flex',flexDirection: 'column',justifyContent: 'center', marginTop: '1rem' }}>
                 <div>{jobInfo.description}</div>
                 <Button variant="primary" style={{margin: '1rem 15rem 0 15rem'}}>Make An Offer</Button>
